60182829c831ab9275d566fa8756eca312699e42
PedroIngro <PedroIngro>
2024-02-26 19:22:18 +0000

    Testes de Trigger

---
+++ notebooks/test_storage_trigger.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+
+# COMMAND ----------
+
+kc_house_data = spark.read \
+    .option("header", True) \
+    .option("inferSchema", True) \
+    .csv("dbfs:/mnt/dados/bronze/kc_house_data.csv")
+
+# COMMAND ----------
+
+kc_house_data_partition = kc_house_data.withColumn('year', F.substring(F.col('date'), 0, 4))
+kc_house_data_partition = kc_house_data_partition.withColumn('year', F.col('year').cast(T.IntegerType()))\
+                                                 .filter(F.col('year') == 2014)
+
+# COMMAND ----------
+
+sink = "dbfs:/mnt/dados/silver/test_storage_trigger"
+kc_house_data_partition.coalesce(1).write.partitionBy('year').save(path=sink, format="parquet", mode="overwrite")
+
+
+# COMMAND ----------
+
+
